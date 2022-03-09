c33df6c5affe33867ff5aca13dc090e900a15f79
kleber <kleberrocha2701@gmail.com>
2022-03-08 21:53:52 -0300

    Curso testando javascript aula 10

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,1 +1,33 @@
-module.exports.queryString = (obj) => new URLSearchParams(obj).toString();
+const AllowdFormat = (value) => value.find(v => (typeof v) === 'object' && !Array.isArray(v));
+
+const convertNestedToFlat = (nestedArray) => {
+
+    return ((nestedArray.join()).split('='))
+        .map((a, i) => {
+            if (i === 1) { return a.split(',') }
+            return a;
+        });
+}
+
+module.exports.queryString = (obj) => {
+    const value = Object.values(obj);
+    if (!AllowdFormat(value)) return new URLSearchParams(obj).toString();
+    throw new Error('Please Check yout parms');
+}
+
+module.exports.parse = (string) => {
+    const arrayKeyValues = string.split('&');
+    const regex = new RegExp(/%2C/gm);
+    const resultArray = arrayKeyValues.map(v => {
+        let nestedArray = [];
+        if (regex.test(v)) {
+            nestedArray = v.split(regex)
+        }
+        if (nestedArray.length) {
+            return convertNestedToFlat(nestedArray);
+        }
+        return v.split('=');
+    });
+
+    return Object.fromEntries(resultArray);
+}
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,11 +1,31 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('queryString.js', () => {
 
     const obj = { name: 'kleber', age: '29' }
+    const obj02 = { name: 'kleber', age: '29', skills: ['Javascript', 'css', 'html'] };
+    const obj03 = { name: 'kleber', age: '29', skills: { fronend: "'Javascript''css', 'html'" } };
 
     it('should create a query string from an object', () => {
         expect(queryString(obj)).toBe('name=kleber&age=29');
+    });
+
+    it('should create a query string from an object even if one of its values ​​is an array', () => {
+        expect(queryString(obj02)).toBe('name=kleber&age=29&skills=Javascript%2Ccss%2Chtml');
+    });
+
+    it('should throw a new error if the entered values ​​contain a nested object value', () => {
+
+        expect(() => {
+            queryString(obj03)
+        }).toThrowError();
+    });
+
+    it.only('should create a object from an query string ', () => {
+
+        expect(parse('name=kleber&age=29')).toEqual({ "age": "29", "name": "kleber" });
+        expect(parse('name=kleber&age=29&skills=Javascript%2Ccss%2Chtml')).toEqual({ "age": "29", "name": "kleber", "skills": ["Javascript", "css", "html",] });
+
     });
 
 });
